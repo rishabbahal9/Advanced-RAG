ce5c76e59a1814e4f1335da40176a4087f08b26e
Rishab Bahal <rishabbahal@gmail.com>
2024-08-07 12:36:42 -0400

    Self RAG implemented!

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,9 +1,14 @@
+from pprint import pprint
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from graph.chains.generation import generation_chain
+from graph.chains.hallucination_grader import hallucination_grader, GradeHallucinations
+
 from ingestion import retriever
 
 
@@ -29,3 +34,34 @@ def test_retrival_grader_answer_no() -> None:
     )
 
     assert res.binary_score == "no"
+
+
+def test_generation_chain() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    pprint(generation)
+
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {
+            "documents": docs,
+            "generation": "In order to make pizza we need to first start with the dough",
+        }
+    )
+    assert not res.binary_score
