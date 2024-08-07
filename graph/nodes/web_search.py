3b099e49dec1639209baa5cee8721d7cc0c5a379
Rishab Bahal <rishabbahal@gmail.com>
2024-08-07 10:16:21 -0400

    corrective RAG implemented!

---
+++ graph/nodes/web_search.py
@@ -0,0 +1,31 @@
+from typing import Any, Dict
+
+from dotenv import load_dotenv
+from langchain.schema import Document
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+from graph.state import GraphState
+
+load_dotenv()
+web_search_tool = TavilySearchResults(max_results=3)
+
+
+def web_search(state: GraphState) -> Dict[str, Any]:
+    print("---WEB SEARCH---")
+    question = state["question"]
+    documents = state["documents"]
+
+    tavily_results = web_search_tool.invoke({"query": question})
+    joined_tavily_result = "\n".join(
+        [tavily_result["content"] for tavily_result in tavily_results]
+    )
+    web_results = Document(page_content=joined_tavily_result)
+    if documents is not None:
+        documents.append(web_results)
+    else:
+        documents = [web_results]
+    return {"documents": documents, "question": question}
+
+
+if __name__ == "__main__":
+    web_search(state={"question": "agent memory", "documents": None})
